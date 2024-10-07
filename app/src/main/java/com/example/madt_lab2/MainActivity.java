752f7eaefc103405670e3594b0f4c6c79d336886
Samuel Class <samuel.palmero@tprs.stud.vu.lt>
2024-10-07 19:57:49 +0300

    Initial Commit, I have added all the text, inputs text and buttons

---
+++ app/src/main/java/com/example/madt_lab2/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.madt_lab2;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.madt_lab2.utilities.TextCounting;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText userInput;
+    Spinner countOption;
+    TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        userInput = findViewById(R.id.userInput);
+        countOption = (Spinner) findViewById(R.id.countOption);
+        tvResult = findViewById(R.id.userInput);
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.countOption, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner.
+        countOption.setAdapter(adapter);
+
+
+    }
+
+    public void onBtnCountClick(View view) {
+        String userInput = String.valueOf(this.userInput);
+        TextCounting tc = new TextCounting();
+        int count = tc.getCharsCount(userInput);
+        tvResult.setText(String.valueOf(count));
+    }
+
+
+}
---
+++ app/src/main/java/com/example/madt_lab2/utilities/TextCounting.java
@@ -0,0 +1,12 @@
+package com.example.madt_lab2.utilities;
+
+public class TextCounting {
+
+    public static int getCharsCount(String userPhrase) {
+        return userPhrase.length();
+    }
+
+    public static int getWordsCount(String userPhrase) {
+        return userPhrase.length();
+    }
+}
